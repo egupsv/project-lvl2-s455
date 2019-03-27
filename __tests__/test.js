aafdba7991c440bbf2178cb6847d06ea769e45ee
egupsv <egupsv@mail.ru>
2019-03-27 22:30:21 +0400

    step 4 err

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,16 +2,9 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('check json', () => {
-  const pathToFileBefore = 'before.json';
-  const pathToFileAfter = 'after.json';
-  const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
-  expect(genDiff(pathToFileBefore, pathToFileAfter)).toBe(expected);
-});
-
-test('check yaml', () => {
-  const pathToFileBefore = 'before.yaml';
-  const pathToFileAfter = 'after.yaml';
+test.each(['json', 'yaml', 'ini'])('check json, yaml, ini', (format) => {
+  const pathToFileBefore = `before.${format}`;
+  const pathToFileAfter = `after.${format}`;
   const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
   expect(genDiff(pathToFileBefore, pathToFileAfter)).toBe(expected);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'fs';
+import ini from 'ini';
 
 const formats = {
   json: filePath => JSON.parse(fs.readFileSync(filePath)),
   yaml: filePath => yaml.safeLoad(fs.readFileSync(filePath)),
+  ini: filePath => ini.parse(fs.readFileSync(filePath)),
 };
 
 export default filePath => formats[path.extname(filePath).slice(1)](filePath);
