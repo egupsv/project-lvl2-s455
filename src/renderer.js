e4f526ad6d5f60f21fa3e6ae4a0d5cce282922b0
egupsv <egupsv@mail.ru>
2019-03-30 17:25:13 +0400

    step 5 renderer almost fixed

--- src/renderer.js
+++ src/renderer.js
@@ -1,27 +1,35 @@
-const render = (data) => {
+const render = (data, depth = 1) => {
+  const tab = 2;
+  const currentTab = tab * depth;
   const plus = data
     .filter(e => e.added)
-    .reduce((acc, e) => [...acc, `  + ${e.added.key}: ${e.added.value}`], [])
-    .join('\n') || '';
+    .reduce((acc, e) => [...acc, `+ ${e.added.key}: ${e.added.value}`], [])
+    .join(`\n${' '.repeat(currentTab)}`) || '';
   const minus = data
     .filter(e => e.deleted)
-    .reduce((acc, e) => [...acc, `  - ${e.deleted.key}: ${e.deleted.value}`], [])
-    .join('\n') || '';
+    .reduce((acc, e) => [...acc, `- ${e.deleted.key}: ${e.deleted.value}`], [])
+    .join(`\n${' '.repeat(currentTab)}`) || '';
   const both = data
     .filter(e => e.added_deleted)
-    .reduce((acc, e) => [...acc, `  + ${e.added_deleted.key}: ${e.added_deleted.valueAfter}`,
-      `  - ${e.added_deleted.key}: ${e.added_deleted.valueBefore}`], [])
-    .join('\n') || '';
+    .reduce((acc, e) => [...acc, `+ ${e.added_deleted.key}: ${e.added_deleted.valueAfter}`,
+      `- ${e.added_deleted.key}: ${e.added_deleted.valueBefore}`], [])
+    .join(`\n${' '.repeat(currentTab)}`) || '';
   const nothing = data
     .filter(e => e.unchanged)
-    .reduce((acc, e) => [...acc, `    ${e.unchanged.key}: ${e.unchanged.value}`], [])
-    .join('\n') || '';
+    .reduce((acc, e) => [...acc, `  ${e.unchanged.key}: ${e.unchanged.value}`], [])
+    .join(`\n${' '.repeat(currentTab)}`) || '';
   const child = data.filter(e => e.complexData).map(e => e.complexData);
   console.log(child);
+  const plain = `{\n${' '
+    .repeat(currentTab)}${nothing}\n${' '
+    .repeat(currentTab)}${both}\n${' '
+    .repeat(currentTab)}${minus}\n${' '
+    .repeat(currentTab)}${plus}\n${' '
+    .repeat(currentTab- tab)}}`;
   if (child.length === 0) {
-    return `{\n${nothing}\n${both}\n${minus}\n${plus}\n}`;
+    return plain;
   }
-  return `  ${child.reduce((acc, e) => [...acc, `${e.key}: {\n`, render(e.children), '\n}'], []).join('')}\n${nothing}\n${both}\n${minus}\n${plus}\n`;
+  return `{\n${' '.repeat(currentTab)}${child.reduce((acc, e) => [...acc, `${e.key}: `, render(e.children, depth + 1), '\n}\n'], []).join('')}${plain}`;
 };
 
 export default render;
