783e366dc8001c43d4ffae82ff4f5f165c94ea34
egupsv <egupsv@mail.ru>
2019-03-31 10:23:14 +0400

    step 5 light simplification of renderer

--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,20 @@
 const render = (data, depth = 1) => {
   const tab = 4;
   const currentTab = tab * depth;
-
+  
   const stringify = obj => (obj instanceof Object ? Object.keys(obj)
     .reduce((acc, e, i) => [...acc, `{\n${' '.repeat(currentTab + tab)}${e}: ${Object.values(obj)[i]}\n${' '.repeat(currentTab)}}`], [])
     .join('\n') : obj);
 
   const arrPlus = data
     .filter(e => e.added)
-    .reduce((acc, e) => [...acc, `+ ${e.added.key}: ${stringify(e.added.value)}`], []);
+    .map((e) => `+ ${e.added.key}: ${stringify(e.added.value)}`);
   const plus = arrPlus.length === 0 ? '' : `${' '.repeat(currentTab - 2)}${arrPlus
     .join(`\n${' '.repeat(currentTab - 2)}`)}\n`;
 
   const arrMinus = data
     .filter(e => e.deleted)
-    .reduce((acc, e) => [...acc, `- ${e.deleted.key}: ${stringify(e.deleted.value)}`], []);
+    .map((e) => `- ${e.deleted.key}: ${stringify(e.deleted.value)}`);
   const minus = arrMinus.length === 0 ? '' : `${' '.repeat(currentTab - 2)}${arrMinus
     .join(`\n${' '.repeat(currentTab - 2)}`)}\n`;
 
@@ -27,7 +27,7 @@ const render = (data, depth = 1) => {
 
   const arrNothing = data
     .filter(e => e.unchanged)
-    .reduce((acc, e) => [...acc, `${e.unchanged.key}: ${stringify(e.unchanged.value)}`], []);
+    .map((e) => `${e.unchanged.key}: ${stringify(e.unchanged.value)}`);
   const nothing = arrNothing.length === 0 ? '' : `${' '.repeat(currentTab)}${arrNothing
     .join(`\n${' '.repeat(currentTab)}`)}\n`;
 
