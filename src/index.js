afb55829a251706dca12c8574b99ba156d3750ef
egupsv <egupsv@mail.ru>
2019-03-31 11:48:54 +0400

    step6 new renderers directory

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 import makeAST from './Astmaker';
-import render from './renderers';
+import render from './renderers/renderers';
 
 const format = filePath => path.extname(filePath).slice(1);
 
-export default (filePathBefore, filePathAfter) => {
+export default (filePathBefore, filePathAfter, outputFormat) => {
   const contentBefore = parse(format(filePathBefore), fs.readFileSync(filePathBefore, 'utf-8'));
   const contentAfter = parse(format(filePathAfter), fs.readFileSync(filePathAfter, 'utf-8'));
-  return render(makeAST(contentBefore, contentAfter));
+  return render(makeAST(contentBefore, contentAfter), outputFormat);
 };
---
+++ src/renderers/plain_renderer.js
@@ -0,0 +1,27 @@
+const render = (data) => {
+
+    const stringify = obj => (obj instanceof Object ? '[complex value]' : obj);
+
+    const updated = data
+      .filter(e => e.added_deleted)
+      .map(e => `Prorepty '${e.added_deleted.key}' was updated. From '${stringify(e.added_deleted.valueBefore)}' to '${stringify(e.added_deleted.valueAfter)}'`)
+      .join('\n');
+    
+    const removed = data
+      .filter(e => e.deleted)
+      .map(e => `Prorepty '${e.deleted.key}' was removed`)
+      .join('\n');
+
+    const addedWith = data
+      .filter(e => e.added)
+      .map(e => `Prorepty '${e.added.key}' was added with value: '${stringify(e.added.value)}'`)
+      .join('\n');
+
+    const child = data.filter(e => e.complexData).map(e => e.complexData).map(e => [e.key, render(e.children)]);
+    
+    if (child.length === 0) {
+      return `${updated}\n${removed}\n${addedWith}`;
+    }
+    return `${updated}\n${removed}\n${addedWith}\n${child}`
+}
+export default render;
